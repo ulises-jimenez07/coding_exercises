2318319db4f7f7f5b55f1d98e1d7ffbaad62f4e9
Ulises Jimenez <ulises.jimenez07@gmail.com>
2025-12-03 12:31:37 -0600

    102 new solution

--- leet_code/trees/102_binary_tree_level_order.py
+++ leet_code/trees/102_binary_tree_level_order.py
@@ -18,6 +18,8 @@ from typing import (
 
 
 class TreeNode:
+    """Binary tree node with value and left/right children."""
+
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
@@ -25,6 +27,8 @@ class TreeNode:
 
 
 class Solution:
+    """BFS approach tracking level numbers with each node."""
+
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root is None:
             return []
@@ -51,10 +55,48 @@ class Solution:
         return answer
 
 
+class Solution2:
+    """Alternative approach: Process entire level at once using queue size."""
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        queue = [root]
+        ans = []
+
+        while queue:
+            # Capture number of nodes at current level
+            level_size = len(queue)
+            level = []
+
+            # Process all nodes at current level
+            for _ in range(level_size):
+                node = queue.pop(0)
+                level.append(node.val)
+
+                # Add children to queue for next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            # Add completed level to result
+            ans.append(level)
+
+        return ans
+
+
 # -----------------------------------------------------------------------------
 
 
 class TestLevelOrder(unittest.TestCase):
+    """Test cases for binary tree level order traversal."""
+
     def setUp(self):
         self.solution = Solution()
 
